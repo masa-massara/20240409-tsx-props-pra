0703bf0a530b5fd7d224ac3a9b182dbcacefa54f
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-04-10 00:00:11 +0900

    オブジェクトと配列の渡し方を習得

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import "./App.css";
 import DashBoard from "./Components/DashBoard";
 
 function App() {
+  const name = { firstName: "山田", lastName: "太郎" };
+  const followers = [
+    { firstName: "山田", lastName: "一郎" },
+    { firstName: "山田", lastName: "二郎" },
+    { firstName: "山田", lastName: "三郎" },
+  ];
   return (
     <>
       {/* propsの値のデータの間違いを防げる、propsの渡し忘れを防げる */}
-      <DashBoard username="my name" notification={10} />
+      <DashBoard username="my name" notification={10} name={name} followers={followers}/>
+      {/* <DashBoard username="my name" notification={10} name={{ firstName: "山田", lastName: "太郎" }} /> */}
+      {/* という形で渡しても出力は同じ */}
     </>
   );
 }
--- src/Components/DashBoard.tsx
+++ src/Components/DashBoard.tsx
@@ -1,6 +1,14 @@
 type UserProps = {
   username: string;
   notification: number;
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  followers: {
+    firstName: string;
+    lastName: string;
+  }[];
 };
 
 const DashBoard = (props: UserProps) => {
@@ -8,7 +16,19 @@ const DashBoard = (props: UserProps) => {
     <div>
       DashBoard
       <h1>{props.username}</h1>
+      <h2>
+        こんにちは！{props.name.firstName} {props.name.lastName}さん
+      </h2>
       <p>進捗の通知が{props.notification}件あります</p>
+      <h2>フォロワー</h2>
+      <div>
+        {props.followers.map((follower) => (
+          <p>
+            {follower.firstName}
+            {follower.lastName}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
